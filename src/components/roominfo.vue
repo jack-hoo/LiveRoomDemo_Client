2b5e812978d8c3fa48a259595fed94b3feae122c
jack-hoo <979783618@qq.com>
2017-06-22 18:00:27 +0800

    截图上传

--- src/components/roominfo.vue
+++ src/components/roominfo.vue
@@ -7,7 +7,6 @@
         <li>个人简历: <a href="/LiveDemo/resume">胡上杰的简历</a></li>
         <li>毕业院校: 江西财经大学软件工程</li>
         <li>技术栈:JavaWeb全栈开发</li>
-        <li>Demo技术详解: <a href="">here</a></li>
         <li>博客地址: <a href="http://blog.csdn.net/hushangjie">csdn</a></li>
         <li>开源项目地址: <a href="https://github.com/jack-hoo">github</a></li>
         <li>状态: 求职中(谢谢各位老板和猎手)</li>
